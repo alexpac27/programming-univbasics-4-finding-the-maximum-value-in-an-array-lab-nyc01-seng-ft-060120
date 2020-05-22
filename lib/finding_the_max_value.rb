1cf9ac15d1efd28dd01b287622df78e7613a62e7
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-22 13:55:13 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,4 +1,4 @@
-#array = [1,2,3,4]
+array = [1,2,3,4]
 
 #def find_max_value(array)
 #  p array.sort[-1]
@@ -7,4 +7,9 @@
 #find_max_value(array)
 
 def find_max_value(array)
+  count == 0
+  highest_i = 0
+  while count < array.length do
+    
+  end
 end
